0101eeb88ad70ac4c52b9078fd7ad4ceb42a68f8
ArjunTeh <arjun_teh@utexas.edu>
2016-10-30 01:38:09 -0500

    added laplacian filter to the code for reference

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
+#get the image into our variables
 img = cv2.imread("/home/arjun/Pictures/bird.jpg")
 cv2.namedWindow("opencv")
 cv2.imshow("opencv",img)
-padded = cv2.copyMakeBorder(img, 1, 1, 1, 1, cv2.BORDER_REPLICATE)
+
+grad_img = cv2.Laplacian(img, cv2.CV_64F)
+
+cv2.imshow("opencv", grad_img)
+
+
 cv2.waitKey(0)
