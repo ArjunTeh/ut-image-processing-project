c61ba50d91073d15da37dd4216521ca7ac48fb75
ArjunTeh <arjun_teh@utexas.edu>
2016-11-04 11:14:58 -0500

    put the seamcarver code into an object form

--- seamcalculations.py
+++ seamcalculations.py
@@ -2,25 +2,43 @@ import math
 import cv2
 import numpy as np
 
-#calculating seams function
-def calculateSeams(energyMap):
-    "calculates the seam energies for the energyMap given"
-    [height, width] = energyMap.shape[:2]
-    verticalSeams = energyMap.copy()
+class SeamCarver:
+    def __init__(self, img):
+        self.img = img
 
-    for rows in range(1, height):
-        for cols in range(0, width):
-            center = energyMap[rows-1, cols]
-            left = energyMap[rows-1, cols-1] if cols > 0 else float('inf')
-            right = energyMap[rows-1, cols+1] if cols < width-1 else float('inf')
-            verticalSeams[rows, cols] = energyMap[rows, cols]
 
-    horizontalSeams = energyMap.copy()
-    for col in range(1, width):
-        for rows in range(0, height):
-            center = energyMap[rows-1, cols]
-            above = energyMap[rows-1, cols-1] if cols > 0 else float('inf')
-            below = energyMap[rows-1, cols+1] if cols < width-1 else float('inf')
-            horizontalSeams[rows, cols] = energyMap[rows, cols]
+    def createEnergyMap(self):
+        #calculate the gradient of the image
+        grad_img = cv2.Laplacian(self.img, cv2.CV_64F)
+        grad_img_abs = np.absolute(grad_img)
 
-    return [verticalSeams, horizontalSeams]
+        #merge the channels to get the energy map
+        b,g,r = cv2.split(grad_img_abs)
+        self.energy_map = cv2.add( cv2.add(b, g), r)
+
+    #calculating seams function
+    def calculateSeams(self):
+        energyMap = self.energy_map
+        "calculates the seam energies for the energyMap given"
+        [height, width] = energyMap.shape[:2]
+        verticalSeams = energyMap.copy()
+
+        for rows in range(1, height):
+            for cols in range(0, width):
+                center = energyMap[rows-1, cols]
+                left = energyMap[rows-1, cols-1] if cols > 0 else float('inf')
+                right = energyMap[rows-1, cols+1] if cols < width-1 else float('inf')
+                verticalSeams[rows, cols] = energyMap[rows, cols]
+
+        horizontalSeams = energyMap.copy()
+        for col in range(1, width):
+            for rows in range(0, height):
+                center = energyMap[rows-1, cols]
+                above = energyMap[rows-1, cols-1] if cols > 0 else float('inf')
+                below = energyMap[rows-1, cols+1] if cols < width-1 else float('inf')
+                horizontalSeams[rows, cols] = energyMap[rows, cols]
+
+        return [verticalSeams, horizontalSeams]
+
+    def getEnergyMap(self):
+        return self.energy_map
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import math
 import cv2
 import numpy as np
-from seamcalculations import calculateSeams
+import seamcalculations as sc
 from matplotlib import pyplot as plt
 
 
@@ -10,19 +10,13 @@ img = cv2.imread("/home/arjun/Pictures/bird.jpg")
 cv2.namedWindow("energy map")
 cv2.namedWindow("original")
 
-#calculate the gradient of the image
-grad_img = cv2.Laplacian(img, cv2.CV_64F)
-grad_img_abs = np.absolute(grad_img)
+seamCarver = sc.SeamCarver(img)
+seamCarver.createEnergyMap()
+seamCarver.calculateSeams()
 
-#merge the channels to get the energy map
-b,g,r = cv2.split(grad_img_abs)
-energy_map = cv2.add( cv2.add(b, g), r)
+energy_map = seamCarver.getEnergyMap()
 energy_map_norm = cv2.normalize(energy_map)
 normal_em = cv2.convertScaleAbs(energy_map)
-
-print energy_map.shape
-
-calculateSeams(energy_map)
 
 cv2.imshow("energy map", normal_em)
 cv2.imshow("original", img)
